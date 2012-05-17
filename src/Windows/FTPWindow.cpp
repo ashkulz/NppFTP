684f0d5986e941a12974db9cd887d4e6eb294295
rameshg87 <rameshg87@users.sourceforge.net>
2012-05-17 17:47:42 +0000

    Fix for bug 3488987.
    git-svn-id: svn://svn.code.sf.net/p/nppftp/code/NppFTP@18 e1eea60f-96cb-45df-9f85-ae3a5fd22bdd

--- src/Windows/FTPWindow.cpp
+++ src/Windows/FTPWindow.cpp
@@ -1210,7 +1210,28 @@ int FTPWindow::CreateFile(FileObject * parent) {
 	if (res != 1)
 		return 0;
 
-	const TCHAR * newName = id.GetValue();
+    const TCHAR * newName = id.GetValue();
+
+    // Check if there is already an existing file of the same name
+    int childcount = parent->GetChildCount();
+    char *newFileName_CP = SU::TCharToCP(newName, CP_ACP);
+
+	for(int i = 0; i < childcount; i++) {
+
+	    const char *currentFileName = parent->GetChild(i)->GetName();
+		if (!strcmp(currentFileName, newFileName_CP)) {
+
+            res = ::MessageBox(m_hwnd, TEXT("A file/directory by the same name already exists. Do you want to create a new blank file ?"), TEXT("Creating file"), MB_YESNO);
+            if (res == IDNO) {
+                return 0;
+            }
+            else {
+                break;
+            }
+        }
+	}
+
+
 	char path[MAX_PATH];
 	res = PU::ConcatLocalToExternal(parent->GetPath(), newName, path, MAX_PATH);
 	if (res == -1)

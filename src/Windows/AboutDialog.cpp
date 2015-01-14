6e2e468fb74509fbdce277ea76eaa4288faa7f8d
Ashish Kulkarni <kulkarni.ashish@gmail.com>
2015-01-15 01:08:04 +0530

    update the copyright years

--- src/Windows/AboutDialog.cpp
+++ src/Windows/AboutDialog.cpp
@@ -57,7 +57,7 @@ INT_PTR AboutDialog::OnInitDialog() {
 	::SetDlgItemText(m_hwnd, IDC_STATIC_NPPFTPVERSION, nppFTPVersion);
 
 	const TCHAR * aboutMessage = 
-		TEXT("NppFTP, Copyright 2010\r\n")
+		TEXT("NppFTP, Copyright 2010-2015\r\n")
 		TEXT("Created by Harry ( harrybharry@users.sourceforge.net )\r\n")
 		TEXT("Maintained by Ashish Kulkarni (kulkarni.ashish@gmail.com)\r\n")
 		TEXT("\r\n")

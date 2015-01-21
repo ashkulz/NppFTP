0831694bb5123074902bdd6320ae9796428a1313
Ashish Kulkarni <kulkarni.ashish@gmail.com>
2015-01-22 00:41:23 +0530

    point to the new homepage

--- src/Windows/AboutDialog.cpp
+++ src/Windows/AboutDialog.cpp
@@ -97,7 +97,7 @@ INT_PTR AboutDialog::OnCommand(int ctrlId, int notifCode, HWND idHwnd) {
 			result = TRUE;
 			break; }
 		case IDC_BUTTON_VISIT: {
-			ShellExecute(NULL, TEXT("open"), TEXT("http://sourceforge.net/projects/nppftp"), NULL, NULL, SW_SHOWNORMAL);
+			ShellExecute(NULL, TEXT("open"), TEXT("http://ashkulz.github.io/NppFTP/"), NULL, NULL, SW_SHOWNORMAL);
 			result = TRUE;
 			break; }
 		case IDC_BUTTON_DONATE: {

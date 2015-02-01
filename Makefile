ashkulz/NppFTP

commit 945163b3dd92fd61a7074b4af8ec823fff34bc43

change build system to ensure that rebuilds use dependency-tracking
This is important, as a change in a header would not have triggered
a rebuild of files which use the header. The idea was taken from [1],
which uses the -MMD flag to g++ to generate this information.

[1] http://stackoverflow.com/questions/11855386/using-g-with-mmd-in-makefile-to-automatically-generate-dependencies

Makefile
@@ -1,21 +1,24 @@
 # Makefile for building NppFTP
 CXX    = i686-w64-mingw32-g++
 AR     = ar rcs
-CFLAGS = -Os -O3 -Wall -Werror -fexpensive-optimizations -DLIBSSH_STATIC -DUNICODE -D_UNICODE
+CFLAGS = -MMD -Os -O3 -Wall -Werror -fexpensive-optimizations -DLIBSSH_STATIC -DUNICODE -D_UNICODE
 LFLAGS = -static -Lobj -L3rdparty/lib -lcomdlg32 -lcomctl32 -luuid -lole32 -lshlwapi -lssh -lssl -lcrypto -lz -lgdi32 -lws2_32
 INC    = -I3rdparty/include -Iinclude -Iinclude/Npp -Iinclude/Windows -Itinyxml/include -IUTCP/include
+RES    = obj/NppFTP.res
 
 TGT    = bin/NppFTP.dll
 TXML_OBJ = $(patsubst tinyxml/src/%.cpp,obj/%.o,$(wildcard tinyxml/src/*.cpp))
 UTCP_OBJ = $(patsubst UTCP/src/%.cpp,obj/%.o,$(wildcard UTCP/src/*.cpp))
 NPP_OBJ  = $(patsubst src/%.cpp,obj/%.o,$(wildcard src/*.cpp)) $(patsubst src/Windows/%.cpp,obj/%.o,$(wildcard src/Windows/*.cpp))
-OBJECTS  = $(TXML_OBJ) $(UTCP_OBJ) $(NPP_OBJ) obj/NppFTP.res
+OBJECTS  = $(TXML_OBJ) $(UTCP_OBJ) $(NPP_OBJ)
+DEPENDS  = ${OBJECTS:.o=.d}
 
+TGT_D      = bin/NppFTPd.dll
 TXML_OBJ_D = $(patsubst tinyxml/src/%.cpp,obj/%_debug.o,$(wildcard tinyxml/src/*.cpp))
 UTCP_OBJ_D = $(patsubst UTCP/src/%.cpp,obj/%_debug.o,$(wildcard UTCP/src/*.cpp))
 NPP_OBJ_D  = $(patsubst src/%.cpp,obj/%_debug.o,$(wildcard src/*.cpp)) $(patsubst src/Windows/%.cpp,obj/%_debug.o,$(wildcard src/Windows/*.cpp))
-OBJECTS_D  = $(TXML_OBJ_D) $(UTCP_OBJ_D) $(NPP_OBJ_D) obj/NppFTP.res
-TGT_D      = bin/NppFTPd.dll
+OBJECTS_D  = $(TXML_OBJ_D) $(UTCP_OBJ_D) $(NPP_OBJ_D)
+DEPENDS_D  = ${OBJECTS_D:.o=.d}
 
 all:     release
 debug:   makedirs $(TGT_D)
@@ -49,10 +52,10 @@ obj/%_debug.o: src/Windows/%.cpp
 obj/%.res: src/Windows/%.rc
 	@echo RES  $< & windres $(INC) -J rc -O coff -i $< -o $@
 
-$(TGT): $(OBJECTS)
+$(TGT): $(OBJECTS) $(RES)
 	@echo LINK $@ & $(CXX) -shared -Wl,--dll $(OBJECTS) -o $@ -s $(LFLAGS)
 
-$(TGT_D): $(OBJECTS_D)
+$(TGT_D): $(OBJECTS_D) $(RES)
 	@echo LINK $@ & $(CXX) -shared -Wl,--dll $(OBJECTS_D) -o $@ $(LFLAGS)
 
 makedirs:
@@ -62,3 +65,6 @@ makedirs:
 clean:
 	@if exist obj rd /s /q obj
 	@if exist bin rd /s /q bin
+
+-include ${DEPENDS}
+-include ${DEPENDS_D}

commit 9e0170009d7f7ed27ccd5f38b6d6824516912c83

fix resources not being linked into the plugin DLL
This was broken since 945163b3dd92fd61a7074b4af8ec823fff34bc43.

Makefile
@@ -62,10 +62,10 @@ obj/%.res: src/Windows/%.rc
 	@echo RES  $< & $(WINDRES) $(INC) -J rc -O coff -i $< -o $@
 
 $(TGT): $(OBJECTS) $(RES)
-	@echo LINK $@ & $(CXX) -shared -Wl,--dll $(OBJECTS) -o $@ -s $(LFLAGS)
+	@echo LINK $@ & $(CXX) -shared -Wl,--dll $(OBJECTS) $(RES) -o $@ -s $(LFLAGS)
 
 $(TGT_D): $(OBJECTS_D) $(RES)
-	@echo LINK $@ & $(CXX) -shared -Wl,--dll $(OBJECTS_D) -o $@ $(LFLAGS)
+	@echo LINK $@ & $(CXX) -shared -Wl,--dll $(OBJECTS_D) $(RES) -o $@ $(LFLAGS)
 
 bin:
 	@mkdir bin
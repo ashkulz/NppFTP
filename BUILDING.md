99787ad0fff10e3cd70a8dcb9d86344d1efbadb8
Ashish Kulkarni <kulkarni.ashish@gmail.com>
2015-06-06 17:37:34 +0530

    refactor the build system to support multiple toolchains
    The only currently supported toolchain in MinGW-w64.

--- BUILDING.md
+++ BUILDING.md
@@ -9,9 +9,9 @@ MinGW-w64
 Cross-compiling from Debian 8 "jessie":
 * Install the necessary packages: `apt-get install python mingw-w64 cmake zip`.
 * Run `./build_3rdparty.py` to download and compile required libraries.
-* Run `make` to compile the code and produce the zipped plugin.
+* Run `make -f Makefile.mingw` to compile and produce the zipped plugin.
 
 Compiling on Windows:
 * Install the latest [MinGW-builds](http://mingw-w64.sourceforge.net/download.php) toolchain.
 * Copy the `3rdparty` folder from the above cross-compilation setup.
-* Run `mingw32-make` to compile the code and produce the zipped plugin.
+* Run `mingw32-make -f Makefile.mingw` to compile and produce the zipped plugin.
--- build_3rdparty.py
+++ build_3rdparty.py
@@ -7,24 +7,34 @@ DEPENDENT_LIBS = {
         'order' : 1,
         'url'   : 'https://openssl.org/source/openssl-1.0.2a.tar.gz',
         'sha1'  : '46ecd325b8e587fa491f6bb02ad4a9fb9f382f5f',
-        'result': ['include/openssl/ssl.h', 'lib/libssl.a', 'lib/libcrypto.a'],
-        'commands': [
-            'perl Configure --openssldir=%(dest)s --cross-compile-prefix=i686-w64-mingw32- no-shared no-asm mingw64',
-            'make', 'make install_sw',
-            # fix CMake <= 3.2 detection, see https://github.com/Kitware/CMake/commit/c5d9a8283cfac15b4a5a07f18d5eb10c1f388505
-            'sed -i "s/ *define OPENSSL_VERSION_NUMBER/define OPENSSL_VERSION_NUMBER/g" %(dest)s/include/openssl/opensslv.h'
-        ]
+        'target': {
+            'mingw-w64': {
+                'result':   ['include/openssl/ssl.h', 'lib/libssl.a', 'lib/libcrypto.a'],
+                # fix CMake <= 3.2 detection, see https://github.com/Kitware/CMake/commit/c5d9a8283cfac15b4a5a07f18d5eb10c1f388505
+                'replace':  [('crypto/opensslv.h', '# define OPENSSL_VERSION_NUMBER', '#define OPENSSL_VERSION_NUMBER')],
+                'commands': [
+                    'perl Configure --openssldir=%(dest)s --cross-compile-prefix=i686-w64-mingw32- no-shared no-asm mingw64',
+                    'make',
+                    'make install_sw'
+                ]
+            }
+        }
     },
 
     'zlib': {
         'order' : 2,
         'url'   : 'http://downloads.sourceforge.net/libpng/zlib-1.2.8.tar.gz',
         'sha1'  : 'a4d316c404ff54ca545ea71a27af7dbc29817088',
-        'result': ['include/zlib.h', 'include/zconf.h', 'lib/libz.a'],
-        'commands': [
-            'make -f win32/Makefile.gcc PREFIX=i686-w64-mingw32-',
-            'cp zlib.h zconf.h %(dest)s/include', 'cp libz.a %(dest)s/lib'
-        ]
+        'target': {
+            'mingw-w64': {
+                'result':   ['include/zlib.h', 'include/zconf.h', 'lib/libz.a'],
+                'commands': [
+                    'make -f win32/Makefile.gcc PREFIX=i686-w64-mingw32-',
+                    'cp zlib.h zconf.h %(dest)s/include',
+                    'cp libz.a %(dest)s/lib'
+                ]
+            }
+        }
     },
 
     'libssh': {
@@ -32,20 +42,28 @@ DEPENDENT_LIBS = {
         'shadow': True,
         'url'   : 'https://git.libssh.org/projects/libssh.git/snapshot/libssh-libssh-0.7.0.tar.gz',
         'sha1'  : 'a3099b960108f3be4ef1c85be38cb3c3be82e3ee',
-        'result': ['include/libssh/libssh.h', 'lib/libssh.a'],
-        'commands': [
-            'cmake -DCMAKE_SYSTEM_NAME=Windows \
-            -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ \
-            -DOPENSSL_INCLUDE_DIRS=%(dest)s/include -DOPENSSL_CRYPTO_LIBRARY=%(dest)s/lib/libcrypto.a \
-            -DWITH_STATIC_LIB=ON -DCMAKE_INSTALL_PREFIX=%(dest)s -DCMAKE_PREFIX_PATH=%(dest)s %(src)s',
-            'make', 'make install'
-        ]
+        'target': {
+            'mingw-w64': {
+                'result':   ['include/libssh/libssh.h', 'lib/libssh.a'],
+                'commands': [
+                    'cmake -DCMAKE_SYSTEM_NAME=Windows \
+                        -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ \
+                        -DOPENSSL_INCLUDE_DIRS=%(dest)s/include -DOPENSSL_CRYPTO_LIBRARY=%(dest)s/lib/libcrypto.a \
+                        -DWITH_STATIC_LIB=ON -DCMAKE_INSTALL_PREFIX=%(dest)s -DCMAKE_PREFIX_PATH=%(dest)s %(src)s',
+                    'make',
+                    'make install'
+                ]
+            }
+        }
     }
 }
 
 # --------------------------------------------------------------- HELPERS
 
 import os, sys, platform, shutil, urllib, hashlib, tarfile
+
+def join_path(*p):
+    return os.path.abspath(os.path.join(*p))
 
 def message(msg):
     sys.stdout.write(msg)
@@ -67,13 +85,13 @@ def rmdir(path):
         shutil.rmtree(path)
 
 def mkdir_p(*paths):
-    path = os.path.join(*paths)
+    path = join_path(*paths)
     if not os.path.exists(path):
         os.makedirs(path)
 
 def download_file(url, sha1, dir):
     name = url.split('/')[-1]
-    loc  = os.path.join(dir, name)
+    loc  = join_path(dir, name)
     if os.path.exists(loc):
         hash = hashlib.sha1(open(loc, 'rb').read()).hexdigest()
         if hash == sha1:
@@ -98,8 +116,8 @@ def download_tarball(url, sha1, dir, name):
     sub = tar.getnames()[0]
     if '/' in sub:
         sub = sub[:sub.index('/')]
-    src = os.path.join(dir, sub)
-    tgt = os.path.join(dir, name)
+    src = join_path(dir, sub)
+    tgt = join_path(dir, name)
     rmdir(src)
     tar.extractall(dir)
     rmdir(tgt)
@@ -115,26 +133,36 @@ def main():
     mkdir_p(dest, 'include')
     mkdir_p(dest, 'lib')
 
+    target = 'mingw-w64' # only supported platform for now
     for library in sorted(DEPENDENT_LIBS, key=lambda x: DEPENDENT_LIBS[x]['order']):
-        cfg = DEPENDENT_LIBS[library]
+        if target not in DEPENDENT_LIBS[library]['target']:
+            print '%s: skipping (not available)' % library
+            continue
+
+        cfg = DEPENDENT_LIBS[library]['target'][target]
         built = True
         for path in cfg['result']:
-            built = built and os.path.exists(os.path.join(dest, path))
+            built = built and os.path.exists(join_path(dest, path))
         if built:
             print '%s: skipping (already built)' % library
             continue
         print '-' * 60, library
-        download_tarball(cfg['url'], cfg['sha1'], build, library)
+        download_tarball(DEPENDENT_LIBS[library]['url'],
+                         DEPENDENT_LIBS[library]['sha1'], build, library)
 
-        if cfg.get('shadow'):
-            rmdir(os.path.join(build, library+'-build'))
-            mkdir_p(os.path.join(build, library+'-build'))
-            os.chdir(os.path.join(build, library+'-build'))
+        if DEPENDENT_LIBS[library].get('shadow'):
+            rmdir(join_path(build, library+'-build'))
+            mkdir_p(join_path(build, library+'-build'))
+            os.chdir(join_path(build, library+'-build'))
         else:
-            os.chdir(os.path.join(build, library))
+            os.chdir(join_path(build, library))
+
+        for location, src, tgt in cfg.get('replace', []):
+            data = open(join_path(build, library, location), 'r').read()
+            open(join_path(build, library, location), 'w').write(data.replace(src, tgt))
 
         for cmd in cfg['commands']:
-            shell(cmd % { 'dest' : dest, 'src': os.path.join(build, library) })
+            shell(cmd % { 'dest' : dest, 'src': join_path(build, library) })
         os.chdir(dest)
         for path in cfg['result']:
             if not os.path.exists(path):

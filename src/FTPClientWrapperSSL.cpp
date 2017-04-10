5217c3dbd2f894a49568cce9e66b97fe18a61f04
Christian Grasser <christian.grasser@live.de>
2017-04-10 23:48:48 +0200

    - correct buffer overflows on malicious ftp server dir structure - correct invalid comparator issue due to not strictly weak ordering impl issue

--- src/FTPClientWrapperSSL.cpp
+++ src/FTPClientWrapperSSL.cpp
@@ -152,7 +152,8 @@ int FTPClientWrapperSSL::GetDir(const char * path, FTPFile** files) {
 		char * utf8name = SU::TCharToUtf8(di.fileName);
 		char nameCpy[MAX_PATH+1];	//buffer used to handle symlinks
 
-		strcpy(nameCpy, utf8name);
+		strncpy(nameCpy, utf8name, MAX_PATH);
+		nameCpy[MAX_PATH] = '\0';
 		SU::FreeChar(utf8name);
 
 		char * linkLocation = strstr(nameCpy, " -> ");
@@ -160,11 +161,12 @@ int FTPClientWrapperSSL::GetDir(const char * path, FTPFile** files) {
 			*linkLocation = 0;
 		}
 
-		strcpy(ftpfile.filePath, path);
+		strncpy(ftpfile.filePath, path, MAX_PATH);
+		ftpfile.filePath[MAX_PATH] = '\0';
 		if (!endslash) {
-			strcat(ftpfile.filePath, "/");
+			strncat(ftpfile.filePath, "/", (MAX_PATH + 1) - strlen(ftpfile.filePath) - 1);
 		}
-		strcat(ftpfile.filePath, nameCpy);
+		strncat(ftpfile.filePath, nameCpy, (MAX_PATH + 1) - strlen(ftpfile.filePath) - 1);
 /*
 		char * fullName = nameCpy;
 		if (linkLocation != NULL) {

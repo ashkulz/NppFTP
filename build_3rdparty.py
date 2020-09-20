937de5eb7bc406993a69d3023a1cff3e740c280b
Christian Grasser <christian.grasser@live.de>
2020-09-20 09:56:35 +0200

    test checkin with _POSIX_SOURCE define

--- build_3rdparty.py
+++ build_3rdparty.py
@@ -88,8 +88,8 @@ DEPENDENT_LIBS = {
     'libssh': {
         'order' : 3,
         'shadow': True,
-        'url'   : 'https://git.libssh.org/projects/libssh.git/snapshot/libssh-b1bbd20dfa8adc784c03fa74d8c81c30671d011b.tar.gz',
-        'sha1'  : '825bfeab4094d774fbf2f75e4114471a35d4ff66',
+        'url'   : 'https://git.libssh.org/projects/libssh.git/snapshot/libssh-dcc0b9d7aa092bd4bc9f50273e40a3777f9f8c5e.tar.gz',
+        'sha1'  : '8f49fef2c3d8f173b5ce0add486015a3439eed8c',
         'target': {
             'mingw-w64': {
                 'result':   ['include/libssh/libssh.h', 'lib/libssh.a'],

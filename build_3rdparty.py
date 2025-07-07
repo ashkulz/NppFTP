dbe596127dd999c22449d53974368dceffa08ecc
Christian Grasser <christian.grasser@live.de>
2025-07-07 07:50:20 +0200

    openssl 3.5.1

--- build_3rdparty.py
+++ build_3rdparty.py
@@ -5,8 +5,8 @@
 DEPENDENT_LIBS = {
     'openssl': {
         'order' : 1,
-        'url'   : 'https://github.com/openssl/openssl/releases/download/openssl-3.5.0/openssl-3.5.0.tar.gz',
-        'sha256'  : '344d0a79f1a9b08029b0744e2cc401a43f9c90acd1044d09a530b4885a8e9fc0',
+        'url'   : 'https://github.com/openssl/openssl/releases/download/openssl-3.5.1/openssl-3.5.1.tar.gz',
+        'sha256'  : '529043b15cffa5f36077a4d0af83f3de399807181d607441d734196d889b641f',
         'target': {
             'mingw-w64': {
                 'result':   ['include/openssl/ssl.h', 'lib/libssl.a', 'lib/libcrypto.a'],

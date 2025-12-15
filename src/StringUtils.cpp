e430ca51b8eefc93dff2047214dacf07871bf032
Christian Grasser <christian.grasser@live.de>
2025-12-15 11:06:19 +0100

    fix issue D:\a\NppFTP\NppFTP\src\StringUtils.cpp(228,1): warning C4700: uninitialized local variable 'pos' used [D:\a\NppFTP\NppFTP\_build\NppFTP.vcxproj]

--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -1,19 +1,19 @@
 /*
-    NppFTP: FTP/SFTP functionality for Notepad++
-    Copyright (C) 2010  Harry (harrybharry@users.sourceforge.net)
+	NppFTP: FTP/SFTP functionality for Notepad++
+	Copyright (C) 2010  Harry (harrybharry@users.sourceforge.net)
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "StdInc.h"
@@ -224,9 +224,10 @@ void SU::free(char * data) {
 
 bool SU::InString(const tstring & source, const tstring & find) {
 	tstring result(source);
+	tstring::size_type pos = 0;
 
 	tstring::size_type pos = result.find(find, pos);
-	
+
 	return pos != tstring::npos ? true : false;
 }
 

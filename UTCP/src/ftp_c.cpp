78d89c836ddf14f5afe00d413c9b8e0cb6793614
Christian Grasser <christian.grasser@live.de>
2025-12-15 13:27:23 +0100

    try to avoid /home/runner/work/NppFTP/NppFTP/UTCP/src/ftp_c.cpp: In member function ‘virtual void CUT_FTPClient::GetInfoInUNIXFormat(CUT_DIRINFOA*)’: /home/runner/work/NppFTP/NppFTP/UTCP/src/ftp_c.cpp:2984:12: warning: ‘char* strncpy(char*, const char*, size_t)’ output may be truncated copying 10 bytes from a string of length 260 [-Wstringop-truncation]  2984 |     strncpy(di->mod, m_szBuf, 10);

--- UTCP/src/ftp_c.cpp
+++ UTCP/src/ftp_c.cpp
@@ -2857,7 +2857,7 @@ void CUT_FTPClient::GetInfoInDOSFormat( CUT_DIRINFOA * di){
     long    value;
 
         //parse and store the directory information
-        
+
         // mod info is not present
         di->mod[0] = '\0';
 
@@ -2980,10 +2980,11 @@ void CUT_FTPClient::GetInfoInUNIXFormat( CUT_DIRINFOA * di){
     }
 
     //CUT_StrMethods::ParseString(m_szBuf," ", 0, di->mod, sizeof(di->mod));
-      
+
     strncpy(di->mod, m_szBuf, 10);
-    
-    //directory  attrib
+    di->mod[10] = 0;
+
+    //directory attribute
     if(m_szBuf[0]=='d' || m_szBuf[0] =='D')
         di->isDir = TRUE;
     else if (m_szBuf[0]=='l' || m_szBuf[0] =='L')
